fc441fcafe4241a8f10391c76b24dbaf4b9656b0
Jonathan Duran <headjd@gmail.com>
2018-05-28 22:08:22 -0400

    Add UserSeeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $user = new User();
+        $user->name = 'Admin';
+        $user->email = 'admin@likespanish.com';
+        $user->password = bcrypt('123456');
+        $user->type = 'admin';
+        $user->save();
+
+        $user = new User();
+        $user->name = 'Student';
+        $user->email = 'student@likespanish.com';
+        $user->password = bcrypt('123456');
+        $user->type = 'student';
+        $user->save();
+
+        $user = new User();
+        $user->name = 'Teacher';
+        $user->email = 'teacher@likespanish.com';
+        $user->password = bcrypt('123456');
+        $user->type = 'teacher';
+        $user->save();
+
+    }
+}
